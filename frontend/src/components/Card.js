e1c643dd14bfa701cad9aa035ba503ee550718b5
Marcin Jarczewski <marcin.jarc@gmail.com>
2024-04-20 18:04:57 +0200

    feat: stretch cards

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -2,13 +2,13 @@ export function Card({ url, title, date, description }) {
   return (
     <a
       href="#"
-      class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+      className="block w-full p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
     >
-      <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+      <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
         {title}
       </h5>
-      <p class="font-normal text-gray-700 dark:text-gray-200">{date}</p>
-      <p class="font-normal text-gray-700 dark:text-gray-400">{description}</p>
+      <p className="font-normal text-gray-700 dark:text-gray-200">{date}</p>
+      <p className="font-normal text-gray-700 dark:text-gray-400">{description}</p>
     </a>
   );
 }
--- frontend/src/components/Galery.js
+++ frontend/src/components/Galery.js
@@ -6,10 +6,12 @@ export function Gallery(props) {
     // const card = cards[0];
     // console.log(card);
     return (
-        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-            {cards.map((card) => (
-                <Card {...card} key={card.key}/>
-            ))}
-        </div>
+      <div className="flex flex-wrap justify-center w-full gap-4">
+        {cards.map((card) => (
+          <div className="w-1/2 md:w-1/3 lg:w-1/4 p-2" key={card.key}>
+            <Card {...card} />
+          </div>
+        ))}
+      </div>
     );
 }
