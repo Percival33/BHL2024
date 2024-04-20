2f229c31d17c2466d8c0dd886b6322e87997ba23
Marcin Jarczewski <marcin.jarc@gmail.com>
2024-04-21 00:38:40 +0200

    fix: add CORS

--- backend/src/infrastructure/api/app.py
+++ backend/src/infrastructure/api/app.py
@@ -1,11 +1,24 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.infrastructure.api.routes import router
 from src.infrastructure.containers import Container
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
 
 container = Container()
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.container = container
 
